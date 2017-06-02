ca1cce30f8d3c8c489093f6ca25748728b6c0f44
Derrick Chambers <djachambeador@gmail.com>
2017-06-02 16:28:23 -0700

    added git ignore stuff

--- fastmarching/quickmarch.py
+++ fastmarching/quickmarch.py
@@ -6,34 +6,41 @@ Created on Tue May 30 16:39:47 2017
 @author: isti_ew
 """
 
-import skfmm
-import numpy as np
-from mpl_toolkits.mplot3d import axes3d
+import contextlib
+
+import os
+
 import matplotlib.pyplot as plt
-from numpy import sin, cos, pi
+import numpy as np
+import skfmm
+import xarray as xr
 from skimage import measure
 from mpl_toolkits.mplot3d import Axes3D
-import xarray as xr
-import contextlib
-import seaborn as sns
+
+# directory path for giffs
+JPEG_DIR = 'jpegs'
 
 # define basic geometry
-DX = 100
+DX = 50
 X = np.arange(0, 2010, DX)
 Y = np.arange(0, 2010, DX)
 Z = np.arange(0, 2010, DX)
 
-ORIGIN = (900, 1000, 1000)  # location of event origin
+ORIGIN = (1100, 1000, 1000)  # location of event origin
 # velocity in the z coordinate
 BACKGROUND_VELOCITY = 6000
-VELOCITIES = [(800, 5000), (1200, 4500), (1800, 3500)]
+VELOCITIES = [(1100, 5000), (1400, 4000), (1700, 2500)]
 
 STOPE_COORDS = [(1000, 1050), (600, 1400), (600, 1400)]
 STOPE_VELOCITY = 500
 
 # a color map based on velocity
-COLOR_MAP = {VELOCITIES[0][1]: 'r', VELOCITIES[1][1]: 'b', 
-             VELOCITIES[2][1]: 'g', STOPE_VELOCITY: '0.5'}
+
+COLOR1 = tuple(np.array([255, 10, 10])/256.)
+COLOR2 = tuple(np.array([255, 10, 10])/256.)
+COLOR3 = tuple(np.array([255, 10, 10])/256.)
+COLOR_MAP = {VELOCITIES[0][1]: COLOR1, VELOCITIES[1][1]: COLOR2, 
+             VELOCITIES[2][1]: COLOR3, STOPE_VELOCITY: '0.2'}
 
 grid_shape = (len(X), len(Y), len(Z))
 
@@ -80,7 +87,8 @@ def get_travel_times(phi, speed):
 def get_fig_and_axis(speed, figsize=(8, 8)):
     """ get the figure and axis objects, then set outputs """
     fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(111, projection='3d')
+#    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.gca(projection='3d')
     yield fig, ax
     # set axis bounds
     ax.set_xbound(0, len(speed.X))
@@ -104,12 +112,12 @@ def plot_velocity_changes(speed, ax=None, **kwargs):
         plot_surface(ax, speed.values, unique, color=color, **kwargs)
     
 
-def plot_orb(dist: xr.DataArray, contour: float, ax=None):
+def plot_orb(dist: xr.DataArray, contour: float, ax=None, **kwargs):
     """ plot the expanding wavefront """
 
     
     ax = plt.figure().add_subplot(111, projection='3d') if ax is None else ax
-    plot_surface(ax, dist.values, contour)
+    plot_surface(ax, dist.values, contour, **kwargs)
     
 
 
@@ -120,36 +128,38 @@ def plot_surface(ax, ar: np.array, contour_value: float, **kwargs):
                     **kwargs)
 
 
+# ------------------------- Run animations
+# make gif directory
+if not os.path.isdir(JPEG_DIR):
+    os.mkdir(JPEG_DIR)
+
+# calculate travel times, get speed vecto with stopes
 speed_with_stope = set_stope(speed)
 travel_times = get_travel_times(phi, speed_with_stope)
 
-ls = np.linspace(travel_times.min(), travel_times.max(), 25)
-for tt in ls[1:-1]:
-    print(f'plotting {tt}')
-        
+NUM_FRAMES = 125
+
+# set travel times
+ls = np.linspace(travel_times.min(), travel_times.max(), NUM_FRAMES + 2)
+
+# set azimuth and elevation params
+azimuths = np.linspace(20, 360, NUM_FRAMES)
+elevations = np.linspace(travel_times.Z.min(), travel_times.Z.max())
+
+for num, tt in enumerate(ls[1:-1]):
     with get_fig_and_axis(speed) as (f, ax):
         # plot velocity layers
         plot_velocity_changes(speed, ax, alpha=.15)
         # plot stope
-        plot_surface(ax, speed_with_stope.values, STOPE_VELOCITY, color='0.5')
+        plot_surface(ax, speed_with_stope.values, STOPE_VELOCITY, color='0.2',
+                     alpha=.3)
         plot_orb(travel_times, tt, ax=ax)
-
-#    plot_orb(travel_times, .2, ax=ax)
-
-
-
-
-
-
-#
-#def fun(x, y, z):
-#    return cos(x) + cos(y) + cos(z)
-#
-#x, y, z = pi*np.mgrid[-1:1:31j, -1:1:31j, -1:1:31j]
-#vol = fun(x, y, z)
-#verts, faces = measure.marching_cubes(vol, 0, spacing=(0.1, 0.1, 0.1))
-#
-
-
-                #cmap='Spectral', lw=0, shade=False)
-#plt.show()
+        ax.view_init(azim=azimuths[num], elev=8)
+    path = os.path.join(JPEG_DIR, f'{num:03d}.jpeg')
+#    plt.subplots_adjust(left=0.01, right=0.011, top=0.011, bottom=0.01)
+    f.set_size_inches(8, 8, forward=True)
+    f.tight_layout()
+    f.savefig(path, dpi=100)
+    
+# ffmpeg command : ffmpeg -r 8 -i %03d.jpeg  myTestMovie.mp4
+    
---
+++ simplex/simplex.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 13:12:55 2017
+
+@author: isti_ew
+"""
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PatchCollection
+
+import seaborn as sns
+
+STARTING_POINTS = np.array([[0, 0], [0, 1], [1, 0]])
+
+
+# STARTING_VALUES
+
+
+def func(point):
+    """ The Rosenbrock function with a=0, b=1:
+    https://en.wikipedia.org/wiki/Rosenbrock_function """
+    x = point[0]
+    y = point[1]
+    return x ** 2 + (y - x ** 2) ** 2
+
+
+class Simplex:
+    """ class to capture behavior of simplex for graphing """
+
+    alpha = 1  # reflection coef.
+    gamma = 2  # expansion coef.
+    rho = 0.5  # contraction coef.
+    sigma = 0.5  # shrink coef
+
+    def __init__(self, points, values_0=None, func=None):
+        # asserts and setups
+        assert values_0 is not None or func is not None
+        if values_0 is None:  # evaluate
+            values_0 = np.array([func(x) for x in points])
+        assert len(points) == len(values_0)
+        # value place-holder
+        self.cvalues_ =values_0.astype(float)
+        self.cpoints = points.astype(float)  # current points
+        self.ppoints = points.astype(float)  # previous points
+
+    # -------------------------------- properties
+    @property
+    def ccentroid(self):
+        return calculate_centroid(self.cpoints)
+
+    @property
+    def pcentroid(self):
+        return calculate_centroid(self.ppoints)
+
+    @property
+    def max_vertex(self):
+        return self.cpoints[np.argmax(self.cvalues_)]
+
+    @property
+    def min_vertex(self):
+        return self.cpoints[np.argmin(self.cvalues_)]
+
+    @property
+    def reflection(self):
+        """ get reflected version of triangle """
+        return self.ccentroid + (self.ccentroid - self.max_vertex) * self.alpha
+
+    @property
+    def expansion(self):
+        """ get the expansion point """
+        return self.ccentroid + (self.reflection - self.ccentroid) * self.gamma
+
+    @property
+    def contraction(self):
+        """ get contraction point """
+        return self.ccentroid + (self.max_vertex - self.ccentroid) * self.rho
+
+    @property
+    def shrink(self):
+        """ get shrunken simplex """
+        ar = np.copy(self.cpoints)
+        lowest_ind = np.argmin(self.cvalues_)
+        for num, subar in enumerate(ar):
+            if num == lowest_ind:
+                continue
+            ar[num] = self.min_vertex + self.sigma * (subar - self.min_vertex)
+        return ar
+
+    # -------------------------------- methods
+    def update_highest_point(self, new_point):
+        ar = np.copy(self.cpoints)
+        ar[int(np.argmax(self.cvalues_)), :] = new_point
+        assert np.all(ar[np.argmax(self.cvalues_)] == new_point)
+        return ar
+
+
+# ------------------------------------- simplex auxiliary methods
+
+def to_polygon(points, **kwargs):
+    """ given points, wrap them in a closed polygon """
+    return Polygon(points, closed=True, **kwargs)
+
+
+def calculate_centroid(array):
+    """ given an array of vertices calculate a centroid """
+    return array.mean(axis=0)
+
+
+# ------------------------- matplot lib plotting stuff
+
+
+def frame_by_frame(points_1, points_2, num=25):
+    """ Given two sets of points that represent polygons, return a new
+    ndarray (with shape = shape(points_1) + (num)) that extrapolates between
+    each point linearly in order to pass to visualization to simulate
+    motion """
+    assert points_1.shape == points_2.shape
+    assert np.any(points_1 != points_2)
+    change_vector = (points_2 - points_1) / float(num)
+    out = np.zeros(tuple([int(num) + 1] + list(np.shape(points_1))))
+    for ind in range(int(num) + 1):
+        out[ind] = points_1 + (ind * change_vector)
+    return out
+
+
+#def save_fig(path, **figwargs):
+#    """ decorator factory to save figure in path directory """
+#    path = os.path.join(os.path.dirname(__file__), path)
+#    if not os.path.exists(path):
+#        os.mkdir(path)
+#
+#    def deco(func):
+#        nonlocal counter
+#        counter = 0
+#
+#        def wrap(self, *args, **kwargs):
+#            f = plt.Figure(**figwargs)
+#            func(*args, **kwargs)
+#            save_path = os.path.join(path, f'{counter:0.3d}.jpeg')
+#            f.savefig(save_path)
+#            counter += 1
+#
+#        return wrap
+#
+#    return deco
+
+
+def make_path(path):
+    path = os.path.join(os.path.dirname(__file__), path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+class PlotPlex2D:
+    """ convenience class for plotting simplex """
+
+    def __init__(self):
+        self.simplex = Simplex(STARTING_POINTS, values_0=np.array([1, 0, 0]))
+        
+    def get_figure(self):
+        fig = plt.Figure(figsize=(1, 1))
+        return fig
+    
+    def set_axis_limits(self, ax):
+        cent_x = self.simplex.ccentroid[0]
+        cent_y = self.simplex.ccentroid[1]
+        
+        ax.set_xlim(cent_x - 1, cent_x + 1)
+        ax.set_ylim(cent_y - 1, cent_x + 1)
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
+    def plot_2dreflection(self):
+        s1 = self.simplex.cpoints
+        s2 = self.simplex.update_highest_point(self.simplex.reflection)
+        self._plot_2D_sequence('Reflection', s1, s2)
+
+    def _plot_2D_sequence(self, path, s1, s2, ax=None):
+        make_path(path)
+        seqs = frame_by_frame(s1, s2)
+        centroid = self.simplex.ccentroid
+        for num, seq in enumerate(seqs):
+            fig = self.get_figure()
+            plt.axes().set_aspect('equal')
+            ax = plt.gca()
+            poly = to_polygon(seq)
+            collection = PatchCollection([poly], alpha=.3, linewidth=2,
+                                         edgecolor='k')
+            ax.add_collection(collection)
+#            import pdb; pdb.set_trace()
+            ax.scatter([centroid[0]], [centroid[1]], color='r')
+            self.set_axis_limits(ax)
+            
+#            if title:
+            plt.title(path, fontsize=20)
+            plt.show()
+
+
+# ----------------------------- Run animations
+pp = PlotPlex2D()
+pp.plot_2dreflection()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
