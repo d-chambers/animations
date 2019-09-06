df1ab4ed40f8ea6ae7df40514af466bace7b3aaf
derrick <djachambeador@gmail.com>
2019-09-05 21:48:50 -0700

    modify animate script

--- stalta/create_animation.sh
+++ stalta/create_animation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Create the animation using ffmeg
-convert -delay 7 -loop 0 stalta_single/*.png output.mp4
+convert -delay 7 -quality 100 -density 300 -loop 0 stalta_single/*.png output.mp4
